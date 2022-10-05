b8ac74082e4baa896cedd4eba65629e19a5cb7a5
ITWILL <07010701aa@gmail.com>
2022-10-05 20:53:26 +0900

    10_05_수

--- src/test/java/com/itwillbs/web/MemberDAOTest.java
+++ src/test/java/com/itwillbs/web/MemberDAOTest.java
@@ -1,5 +1,7 @@
 package com.itwillbs.web;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.junit.Test;
@@ -9,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.itwillbs.domain.MemberVO;
 import com.itwillbs.persistance.MemberDAO;
 import com.itwillbs.persistance.MemberDAOImpl;
 
@@ -36,7 +39,7 @@ public class MemberDAOTest {
 	private MemberDAO dao;
 	
 	
-	@Test
+//	@Test
 	public void daoTest() {
 		log.info("(♥♥♥♥♥ MemberDAOTest_daoTest)  dao: " + dao);
 		// dao 있네~~~^^ 
@@ -44,9 +47,151 @@ public class MemberDAOTest {
 	}
 	
 	
-	@Test
+	
+//	@Test
 	public void DB_시간_정보_조회() {
 		log.info("(♥♥♥♥♥ MemberDAOTest_DB_시간_정보_조회) dao.getTime: " + dao.getTime());
 	}
 	
-}
+	
+	
+	@Test
+	public void 횐_가입_테서터() {
+//		log.info("(♥♥♥♥♥ MemberDAOTest_횐_가입_테서터) dao.insertMember: " + dao.insertMember(vo) );
+		log.info("(♥♥♥♥♥ MemberDAOTest_횐_가입_테서터) 회원가입 테스트 실행됨");
+		log.info("(♥♥♥♥♥ MemberDAOTest_횐_가입_테서터)  --> DAOImpl 부를거임^^ ↓↓↓↓↓");
+		
+		MemberVO vo = new MemberVO(); // 얘는 강한 결합 아녀요??? ㄱ-  객체 주입해야하는 거 아닌감 
+										// --> ㄴㄴ 주입할 필요 없음!! 매번 쓰는 것도 아니고~~~ 
+		vo.setUserid("pooh");
+		vo.setUserpw("pppp");
+		vo.setUsername("푸우");
+		vo.setUseremail("pooh@pooh.com");
+		
+		dao.insertMember(vo);
+		
+	} 
+	// 1. 회원가입 insertMember Test 끝
+	
+	
+	
+	// 2. 로그인 체크 -- 입력받은 정보를 DB 값과 비교 
+//	@Test
+	public void 로그인_로직_테서터() {
+		log.info("(♥♥♥♥♥ MemberDAOTest_로그인_로직_테서터) --> DAOImpl 부를 거임 ↓↓↓↓↓");
+		
+		MemberVO vo = new MemberVO();
+		vo.setUserid("admin");
+		vo.setUserpw("12345");
+		
+//		MemberVO resultVO = dao.loginMember(vo); // 리턴타입이 MemberVO인 메서드^^ loginMember1
+		MemberVO resultVO = dao.loginMember(vo.getUserid(), vo.getUserpw()); // loginMember2
+		
+		if(resultVO == null) {
+			log.info("(♥♥♥♥♥ MemberDAOTest_로그인_로직_테서터) 회원정보 없음,, 로그인 실패");
+		} else {
+			log.info("(♥♥♥♥♥ MemberDAOTest_로그인_로직_테서터) 회원정보 있음,, 로그인 성공!!!");
+			log.info("(♥♥♥♥♥ MemberDAOTest_로그인_로직_테서터) resultVO: " + resultVO);
+		}
+		
+	}
+	// 2. 로그인 체크 loginMember Test 끝
+	
+	
+	
+	// 3. 회원 정보 조회(아이디만 사용)
+//	@Test
+	public void 회원_정보_조회() {
+		log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_조회) --> DAOImpl 부를 거임 ↓↓↓↓↓");
+		
+		// DAO 객체 생성,, 할 필요 없음!! 젤 위에 dao 객체 주입받았기 때문에 이미 생성되어있는 상태
+		
+		// dao 안에 회원 정보 조회 메서드(리턴타입 MemberVO) 호출
+		MemberVO vo = dao.getMember("haha");
+		
+		if( vo != null) {
+			// vo가 null이 아니다 == 데이터가 있다 == 아이디에 해당하는 정보가 DB에 있다~~
+			
+			// 확인(콘솔에 출력해보기^^)
+			log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_조회) 아이디: " + vo.getUserid());
+			log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_조회) 비번: " + vo.getUserpw());
+			log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_조회) 이름: " + vo.getUsername());
+			log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_조회) 이메일: " + vo.getUseremail());
+			log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_조회) 가입일: " + vo.getRegdate());
+			log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_조회) 정보수정일: " + vo.getUpdatedate());
+			
+		}
+		
+	}
+	// 3. 회원 정보 조회 getMember 끝
+	
+	
+	
+	// 4. 회원 정보 수정 - 아이디, 비밀번호가 같을 때 이메일 수정 (+ 정보 수정일 updatedate도 업뎃되게)
+//	@Test
+	public void 회원_정보_수정() {
+		log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_수정) --> DAOImpl 부를 거임 ↓↓↓↓↓");
+		
+		MemberVO uvo = new MemberVO();
+		uvo.setUserid("admin"); // 기존
+		uvo.setUserpw("12344"); // 기존
+		uvo.setUseremail("admingg@admin.com"); // 수정할 데이터 입력
+		
+		int result = dao.updateMember(uvo);
+		
+		if(result==1) {
+			// 수정 완
+			log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_수정) 수정 성공 uvo: " + uvo);
+			
+		} else {
+			// 수정 실패
+			log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_수정) 수정 실패ㅠ uvo: " + uvo);
+		}
+		
+	}
+	// 4. 회원 정보 수정 updateMember 끝
+
+	
+	
+	// 5. 회원 정보 삭제
+//	@Test
+	public void 회원_정보_삭제() {
+		log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_삭제) --> DAOImpl 부를 거임 ↓↓↓↓↓");
+		
+		MemberVO dvo = new MemberVO();
+		dvo.setUserid("haha");
+		dvo.setUserpw("h");
+		
+		int result = dao.deleteMember(dvo);
+		
+		if(result == 1) {
+			log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_삭제) 삭제 성공 dvo: " + dvo);
+			
+		} else {
+			log.info("(♥♥♥♥♥ MemberDAOTest_회원_정보_삭제) 삭제 실패 dvo: " + dvo);
+			
+		}
+			
+	}
+	// 5. 회원 정보 삭제 끝
+	
+	
+	
+	// 6. 전체 회원 목록 조회	
+//	@Test
+	public void 전체_회원_목록_조회() {
+		log.info("(♥♥♥♥♥ MemberDAOTest_전체_회원_목록_조회) --> DAOImpl 부를 거임 ↓↓↓↓↓");
+		
+		// DAO 전체 회원 목록 조회 메서드 호출
+		List<MemberVO> memberList = dao.getMemberList();
+		
+		for (MemberVO vo : memberList) {
+			log.info("(♥♥♥♥♥ MemberDAOTest_전체_회원_목록_조회) 아이디: " + vo.getUserid());
+		}
+		
+	}
+	// 6. 전체 회원 목록 조회 끝
+	
+	
+	
+}// MemberDAOTest
--- src/test/java/com/itwillbs/web/MysqlConnectTest.java
+++ src/test/java/com/itwillbs/web/MysqlConnectTest.java
@@ -2,6 +2,7 @@ package com.itwillbs.web;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,102 +10,102 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MysqlConnectTest {
-	// 테스트 클래스 : 서버를 사용해서 테스트 하는 동작을
-	//  jUnit을 사용해서 대신 테스트 하는 클래스
+	// 테스트 클래스: 
+	// 서버를 사용해서 테스트하던 동작을,, jUnit을 사용해서 대신 테스트하는 클래스
 	
-//	private static final Logger mylog 
-//	              = LoggerFactory.getLogger(MysqlConnectTest.class);
+	// @Test: 테스트 동작을 수행하는 메서드에 사용
+	//        @Test가 있어야만!!! jnit 실행 가능!! 쟤 주석 처리해보면 앎^^
+	//        객체 생성,, 메서드 호출,, 할 필요 없이!! 이 주석 하나로 실행이 된다~!! 
+	//        jnit 행님이 대신 실행해준다~ 댕큐 댕큐~~
+	//        근데 순서는,, junit 지맘대로~~~ 작성 순서가 실행 순서를 보장하지 않음!! 랜덤임..
+	//        순서 설정하고 싶으면 @Before나 @After 추가~~
+	
+	// @Before: 테스트 작업 전에!! 반드시 준비(실행)되어야 하는 동작을 처리하는 annotation
+	// @After: 테스트 작업 후에!! 반드시 처리하는 동작
+	
+	// 테스트에서 junit 실행 순서
+	// : @Before 먼저 실행 --> @Test 찾아가서 실행 --> @After 실행
 	
 	
-
+	// 인제 sysout 안 쓰고,, logger 쓸 거임,,,, 
+//	private static final Logger mylog =  LoggerFactory.getLogger(MysqlConnectTest.class);
+	// ㄴmylog 단축키 만들고 옴^^
 	private static final Logger log = LoggerFactory.getLogger(MysqlConnectTest.class);
 	
-	// @Test : 테스트 동작을 수행하는 메서드에 사용
-	//        @Test가 있어야지만, junit이 실행 가능
 	
-	// @Before : 테스트 작업전에 반드시 준비(실행)되어야 하는 동작을
-	//  처리하는 어노테이션
-	// @After : 테스트 작업후에 반드시 처리하는 동작 
+	@Test
+	public void test() throws Exception {
+		System.out.println("(MysqlConnectTest) 1.test() 호출됨");
+		System.out.println("(MysqlConnectTest) 테스트 클래스 실행!!!!!!!\n");
+		// 실행하려면,, 객체 생성하고~~~ .test()해서 불렀어야 됐는데,, 그 럴 필 요 없 다 ㄷ ㄷ 
+	}
 	
-	// * @Test : 작성순서가 실행순서를 보장 X (랜덤)
 	
-	// 테스트에서 JUnit 실행 순서
-	// @Before   ->   @Test   -> @After
-	
-	//@Test
+//	@Test      // 우와~~ 한글 이름도 삽가능~~^^
 	@Before
-	public void 테스트동작하는지테스트() throws Exception{
-		System.out.println(" MysqlConnectTest - test() 호출 ");
-		System.out.println(" 테스트 클래스 실행!! ");
+	public void 테서터동작하나보는테서터ㅋ() throws Exception {
+		System.out.println("(MysqlConnectTest) 2.테서터동작하나보는테서터ㅋ() 호출됨");
+		System.out.println("(MysqlConnectTest) 테스트 클래스 실행!!!!!!!\n");
 	}
 	
 	
 	@Test
-	public void 디비연결테스트() throws Exception{
-		System.out.println(" 디비연결테스트() 수행 ");
+	public void 디비연결테스트() throws Exception {
+		System.out.println("(MysqlConnectTest) 3.디비연결테스트() 호출됨");
+		System.out.println("(MysqlConnectTest) 테스트 클래스 실행!!!!!!!\n");
 		
+		// 디비 연결하기~
 		// 1. 드라이버 로드
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		System.out.println(" T : 드라이버 로드 성공! ");
+		System.out.println("(MysqlConnectTest) 3.디비연결Test: 드라이버 로드 성공");
 		
-		// 2. 디비 연결
+		// 2. DB 연결
 		Connection con = DriverManager.getConnection(
-						"jdbc:mysql://localhost:3306/springdb",
-						"root",
+						"jdbc:mysql://localhost:3306/springdb", 
+						"root", 
 						"1234");
-		System.out.println(" T : 디비연결 성공! ");
-		System.out.println(" T : con : "+con);
+		System.out.println("(MysqlConnectTest) 3.디비연결Test: DB 연결 성공~~ con: " + con);
+		// 지금까지 우리,, 이런 작업들 실제로 서버 돌려서 확인했었지요? 
+		// 근데 지금은,, 서버 안 돌리고!!! junit이 대신 서버역할 하면서 연결도 해보고~ DB 접속도 해보고~~~ 캬 좋네
+
+//		mylog.info("3.디비연결Test: 실행되나? 테스트");
+		log.info("3.디비연결Test: 실행되나? 테스트");
 		
-		log.info(" 실행 테스트 ");
 	}
 	
 	
 	
 	@Test
-	public void 디비연결테스트2() throws Exception{
-		System.out.println(" 디비연결테스트2() 수행 ");
+	public void 디비연결테스트2() throws Exception {
+		System.out.println("(MysqlConnectTest) 디비연결테스트2() 호출됨");
+		System.out.println("(MysqlConnectTest) 테스트 클래스 실행!!!!!!!\n");
 		
 		// 1. 드라이버 로드
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		System.out.println(" T : 드라이버 로드 성공! ");
+		System.out.println("(MysqlConnectTest) 디비연결Test2: 드라이버 로드 성공");
+
+		// try-with 구문
+		//    try(자원해제가 필요한 객체 사용, AutoCloseable 인터페이스 객체를 상속하고 있는 객체를 여기다 써주세요){
+		//          예외 발생할만한 코드
+		//    } catch (Exception e) {
+		//          예외 처리 코드
+		//    }
 		
-		// 2. 디비 연결
-//		Connection con = null;
-//		try {
-//			 con = DriverManager.getConnection(
-//					"jdbc:mysql://localhost:3306/springdb",
-//					"root",
-//					"1234");
-//	
-//				System.out.println(" T : 디비연결 성공! ");
-//				System.out.println(" T : con : "+con);
-//		}catch(Exception e) {
-//			e.printStackTrace();
-//		} finally {
-//			con.close();
-//		}
-			// try-with 구문  
-			//		try(자원해제가 필요한 객체 사용,AutoCloseable인터페이스 객체를 상속하는 객체){
-			//			예외 발생할만 코드
-			//		} catch (Exception e) {
-		    //		    예외 처리코드
-			//		}
-		
-		try(Connection con = DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/springdb",
-				"root",
-				"1234")) {
-	
-				System.out.println(" T : 디비연결 성공! ");
-				System.out.println(" T : con : "+con);
-		}catch(Exception e) {
-			e.printStackTrace();
-		} 
-		
-		
-		log.info(" 실행 테스트 ");
-	}
-	
-	
+
+		// try-with 구문 써서.. << 자원해제 알아서 해줌!! try 괄호 안에 그 객체 적으면,, 나오게
+		// 디비 연결하기~
+		try (Connection con = DriverManager.getConnection(
+								"jdbc:mysql://localhost:3306/springdb", 
+								"root", 
+								"1234")) {
+			
+			System.out.println("(MysqlConnectTest) 디비연결Test2: DB 연결 성공~~ con: " + con);
+			
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			log.info("디비연결Test2: 실행되나? 테스트");
+	} // 디비연결테스트2 끝
 	
 }
